99bbd788dca565487f0dd1b4bb2c08cb87bc2d8a
tyrictjones <107770357+tyrictjones@users.noreply.github.com>
2025-04-25 16:40:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+    function showPDF() {
+      document.getElementById('pdfModal').style.display = 'block';
+    }
+
+    function closePDF() {
+      document.getElementById('pdfModal').style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+      const modal = document.getElementById('pdfModal');
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    }
+    
+    function showFHIR(patientName) {
+    document.getElementById("fhirPatientName").textContent = patientName;
+    document.getElementById("fhirModal").style.display = "block";
+ 		}
+
+  function closeFHIR() {
+    document.getElementById("fhirModal").style.display = "none";
+  	}
+
+  window.onclick = function(event) {
+    const modal = document.getElementById("fhirModal");
+    if (event.target === modal) {
+      modal.style.display = "none";
+   	}
+  }
